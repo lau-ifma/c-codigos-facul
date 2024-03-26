e42210f918538b468ff8790f24bde6cc87dd40e3
Lauanderson <161143157+lau-ifma@users.noreply.github.com>
2024-03-26 00:22:20 -0300

    Add files via upload

---
+++ lista 8/questao2.c
@@ -0,0 +1,23 @@
+
+// incompleto
+#include <stdio.h>
+
+void main(){
+    int N, n, d, A;
+    printf("Digite o valor de (N): ");
+    scanf("%d", &N);
+
+    n = N;
+    d = 1;
+    A = 0; 
+    
+    while (d<= N)
+    {
+        A = A + (n/d);
+        n-= 1;
+        d+= 1;  
+    }
+
+    printf("Valor de A = %i", A );
+
+}
